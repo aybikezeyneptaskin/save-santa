bc9bd75e7574b0aa635f466392ad6f81f3964ca6
buldubu <buldubu@gmail.com>
2022-12-24 18:22:59 +0300

    Cloud destroy added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,11 @@ public class Player : MonoBehaviour
 {
     Transform tr;
     public Rigidbody2D rb;
-    private BoxCollider2D boxcollider;
 
-    public Transform groundCheck;
-    public float groundCheckRadius;
-    public LayerMask whatIsGround;
-    private bool onGround;
     public float speed;
     public float jumpHeight2;
     public float jumpHeight1;
+    public float cloudDestroyTime;
     Vector2 input;
     float jumpLimit = 2;
 
@@ -22,24 +18,21 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         tr = GetComponent<Transform>();
-        boxcollider = GetComponent<BoxCollider2D>();
     }
 
    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            Debug.Log("Collision normal " + collision.contacts[0].normal.y);
-            if (collision.contacts[0].normal.y != -1)
-            {
-                jumpLimit = 2;
-            }
+        Debug.Log("Collision normal " + collision.contacts[0].normal.y+ collision.gameObject.tag);
+        if (collision.gameObject.layer == 6 && collision.contacts[0].normal.y != -1){
+            jumpLimit = 2;
+        }else if(collision.gameObject.layer == 7 && collision.contacts[0].normal.y == 1){
+            jumpLimit = 2;
+            Object.Destroy(collision.gameObject, cloudDestroyTime);
         }
     }
 
     void Update()
     {
-
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
         if(Input.GetKeyDown(KeyCode.Space) && jumpLimit == 2){
             rb.AddForce(Vector2.up * jumpHeight2, ForceMode2D.Impulse);
@@ -56,8 +49,4 @@ public class Player : MonoBehaviour
      
     }
 
-    //private bool IsOnGround()
-    //{
-    //    return Physics2D.BoxCast(boxcollider.bounds.center, boxcollider.bounds.size, 0f, Vector2.down, .1f, whatIsGround);
-    //}
 }
